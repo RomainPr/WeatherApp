8cf1c7598c90c5aa0368652fd07c00899b856213
Rom <romainprevostdev@gmail.com>
2021-06-03 16:20:48 +0200

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const App = () => (
+  <div className="app" />
+);
+
+// == Export
+export default App;
